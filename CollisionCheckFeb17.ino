734f769cb335b9b2d1caae87cdd6477f022ba1b9
fjl2002 <78870830+fjl2002@users.noreply.github.com>
2021-02-17 11:23:12 -0800

    Update CollisionCheckFeb17.ino

--- CollisionCheckFeb17.ino
+++ CollisionCheckFeb17.ino
@@ -37,7 +37,26 @@ void CollisionCheck(int CurrentRow, int CurrentCol){
 }
 
 
+void RowClear_check(int CurrentRow) { //this function will check if a row is full, so that it clears the row and moves the above rows down
+  
+  int FullRow[1][20] = {{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}};
+  int EmptyRow[1][20];
+  
+  int i;
+  int j;
 
+  for (j = 0; j < 20; j++) {
+    if (Background[CurrentRow][j] == FullRow[0][j]) {
+      for (i = 0; i < CurrentRow; i++) {
+        for (j = 0; j <20; j++) {
+          Background[i + 1][j + 1] = Background[i][j];
+          Background[0][j] = EmptyRow[0][j];
+        }
+      }
+    }
+  }
+  
+}
 
 
 
