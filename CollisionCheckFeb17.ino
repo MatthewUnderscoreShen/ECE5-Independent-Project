269616415ca77837215705dbe34f60e49a5848fb
fjl2002 <78870830+fjl2002@users.noreply.github.com>
2021-02-24 09:30:54 -0800

    Update CollisionCheckFeb17.ino

--- CollisionCheckFeb17.ino
+++ CollisionCheckFeb17.ino
@@ -1,4 +1,3 @@
-
 int Background[40][20];
 int TetrisPiece[2][4]; //placeholder tetris piece, put real pieces later
 
@@ -19,6 +18,8 @@ void Piece_to_background(int piece[2][4],int row_Num,int col_Num) {
 void CollisionCheck(int CurrentRow, int CurrentCol){
 
   int EmptySpace[2][4] = {{0,0,0,0},{0,0,0,0}};
+
+  int RandomVar = random(1,8);
   
   int i;
   int j;
@@ -29,34 +30,20 @@ void CollisionCheck(int CurrentRow, int CurrentCol){
         CurrentRow = CurrentRow + 1; //put some stuff in here to move the TetrisPiece down one i somehow, implement millis function
       }
       else if (Background[CurrentRow + i][CurrentCol + j] != EmptySpace[i][j]) {
-        Piece_to_background(TetrisPiece, CurrentRow - 1, CurrentCol); //places the tetris piece in the background matrix
-                                                    //one row above where there is no more space in the background
-      }
-    }
-  }
-}
-
-
-void RowClear_check(int CurrentRow) { //this function will check if a row is full, so that it clears the row and moves the above rows down
-  
-  int FullRow[1][20] = {{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}};
-  int EmptyRow[1][20];
-  
-  int i;
-  int j;
-
-  for (j = 0; j < 20; j++) {
-    if (Background[CurrentRow][j] == FullRow[0][j]) {
-      for (i = 0; i < CurrentRow; i++) {
-        for (j = 0; j <20; j++) {
-          Background[i + 1][j + 1] = Background[i][j];
-          Background[0][j] = EmptyRow[0][j];
+        Piece_to_background(TetrisPiece, CurrentRow - 1, CurrentCol); //places the tetris piece in the background matrix one row above where there is no more space in the background
+        PieceRandomizer(RandomVar);    //fucntion I wrote in another file        
+        //for (i= 0; i < 2; i++) {    (this might not be needed)
+          //for (j = 0; j < 4; j++) {
+            //TetrisPiece[i][j] = NewPiece[i][j]; //implement NewPiece as the result from SevenSided function instead of matrixPrinter
+          }
         }
       }
     }
   }
-  
 }
+
+
+
 
 
 
