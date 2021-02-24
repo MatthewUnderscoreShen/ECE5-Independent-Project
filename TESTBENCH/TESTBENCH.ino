a689b7758d6f0bc620d9fac752c63313cd8314d6
MatthewUnderscoreShen <MatthewShen02@Gmail.com>
2021-02-24 11:48:22 -0800

    piece falling works

--- TESTBENCH/TESTBENCH.ino
+++ TESTBENCH/TESTBENCH.ino
@@ -6,11 +6,13 @@
 
 double x, y; //joystick inputs
 int a, b; //button inputs
-int refresh = 250; //game refresh rate in milliseconds
+int refresh = 1000; //game refresh rate in milliseconds
+int fallSpeed = 2; //How many frames it takes to fall one block
 int pieceNum = 0; //piece number
 int lastPiece;
 int t = 0;
-bool gameState = false;
+int tLast = 0;
+bool gameState = true;
 bool activePiece = false;
 
 int board[40][20];
@@ -27,31 +29,31 @@ typedef struct piece Piece;
 //Initializing pieces.
 // 0:I  1:O  2:T  3:J  4:L  5:S  6:Z
 Piece pieces[] = {
-      (Piece){-1, 1, 8, 40, {
+      (Piece){-1, 1, 8, 0, {
                 {-3, -1, 1, 3}, //x
                 { 1,  1, 1, 1}  //y
              }}, //I piece
-      (Piece){1, 1 , 8, 38, { 
+      (Piece){1, 1 , 8, 2, { 
                 {-1, -1,  1, 1},
                 {-1,  1, -1, 1} 
              }}, //O piece
-      (Piece){0, 0, 8, 38, {
+      (Piece){0, 0, 8, 2, {
                 {0, -2, 0, 2},
                 {0,  0, 2, 0}
              }}, //T piece
-      (Piece){0, 0, 8, 38, {
+      (Piece){0, 0, 8, 2, {
                 {0, -2, -2, 2},
                 {0,  2,  0, 0}
              }}, //J piece
-      (Piece){0, 0, 8, 38, {
+      (Piece){0, 0, 8, 2, {
                 {0, -2, 2, 2},
                 {0,  0, 0, 2}
              }}, //L piece
-      (Piece){0, -1, 8, 38, {
+      (Piece){0, -1, 8, 2, {
                 {-2,    0,   0,   2},
                 {-0.5, -0.5, 0.5, 0.5}
              }}, //S piece
-      (Piece){0, -1, 8, 38, {
+      (Piece){0, -1, 8, 2, {
                 {-2,   0,    0,    2},
                 { 0.5, 0.5, -0.5, -0.5}
              }}  //Z piece
@@ -74,6 +76,8 @@ int rots[][2][2] =
     for (int i = 0; i < 800; i++) { //initalizes the board
       board[i / 20][i % 20] = 0;
     }
+
+    
   }
 
   void loop() {
@@ -85,42 +89,35 @@ int rots[][2][2] =
     b = digitalRead(buttonB);
 
     if (gameState == false) {
-      if (t % 5000 == 0) {
+      if (t % 5000 == 0 && tLast != t) {
+        Serial.println(t);
         Serial.println("Game currently inactive");
       }
-    }
-    if (t % refresh == 0) {
-      setPiece();
-      movePiece();
-    }
-    setPiece(); //Sets the piece if not already set
-    //At this point the piece should be active and initial position set
-
-    if (gameState == false) { //Check for game over
+      tLast = t;
       return;
     }
-    
-    //movePiece(); //Moves and/or rotates the piece.
 
-    /*
-    for (int i = 0; i < 8; i++) {
-      if (i%4 == 0) { Serial.println(""); }
-      Serial.print(currentPiece.ori[i/4][i%4]);
-      Serial.print("  ");
+    //
+    //GAME LOOP START------------------------------
+    //
+    if (t % refresh == 0 && tLast != t) { //Game loop
+      setPiece(); //Places piece on board
+      
+      if (gameState == false) { //checks for game over
+        Serial.println("Game Over");
+        tLast = t;
+        return;
+      }
+      
+      movePiece(t); //moves piece
+
+      //printBoard();
     }
-    Serial.println("");
-    */
-    Serial.println(t);
+    //
+    //GAME LOOP END------------------------------------
+    //
 
-    /*
-    Serial.print(a);
-    Serial.print("\t");
-    Serial.print(b);
-    Serial.print("\t");
-    Serial.print(x);
-    Serial.print("\t");
-    Serial.println(y);
-    */
+    tLast = t;
   }
 
   void setPiece() { //this function chooses and sets the piece
@@ -134,7 +131,7 @@ int rots[][2][2] =
       currentPiece.ori[i/4][i%4] = pieces[pieceNum].ori[i/4][i%4];
     }
 
-    if (checkCollision) {
+    if (checkCollision()) {
       Serial.println("Game Over");
       gameState = false;
       return;
@@ -143,15 +140,66 @@ int rots[][2][2] =
     activePiece = true;
   }
 
-  void movePiece() {
-    //currentPiece
+  void movePiece(int currentT) {
+    if ((currentT / refresh % fallSpeed == 0) && currentPiece.yPos < 40) { //Fall
+      currentPiece.yPos = currentPiece.yPos + 2;
+    }
+
   }
 
   bool checkCollision() { //Checks to see if the current block is colliding with a placed block
     for (int i = 0; i < 4; i++) {
       if (board[ currentPiece.xPos + currentPiece.xOff + currentPiece.ori[0][i] ][ currentPiece.yPos + currentPiece.yOff + currentPiece.ori[1][i] ] == 1) {
-        return false;
+        return true;
       }
     }
-    return true;
+    return false;
+  }
+
+  void printBoard() {
+    Serial.println("------------------------------");
+    for (int i = 0; i < 40; i = i + 2) {
+      for (int j = 0; j < 20; j = j + 2) {
+        Serial.print(" ");
+        if (currentPiece.xPos + currentPiece.xOff + currentPiece.ori[0][0] == j && currentPiece.yPos + currentPiece.yOff + currentPiece.ori[1][0] == i) {
+          Serial.print("1");
+        } else if (currentPiece.xPos + currentPiece.xOff + currentPiece.ori[0][1] == j && currentPiece.yPos + currentPiece.yOff + currentPiece.ori[1][1] == i) {
+          Serial.print("1");
+        } else if (currentPiece.xPos + currentPiece.xOff + currentPiece.ori[0][2] == j && currentPiece.yPos + currentPiece.yOff + currentPiece.ori[1][2] == i) {
+          Serial.print("1");
+        } else if (currentPiece.xPos + currentPiece.xOff + currentPiece.ori[0][3] == j && currentPiece.yPos + currentPiece.yOff + currentPiece.ori[1][3] == i) {
+          Serial.print("1");
+        } else {
+          Serial.print(board[i][j]);
+        }
+        Serial.print(" ");
+      }
+      Serial.println("");
+    }
+    Serial.println("------------------------------");
+  }
+
+  boolean boundsCheck(int dx, int dy, int[][] dRot) { //returns true if proposed transformation puts piece out of bounds
+    int xNew;
+    int yNew;
+    int[][] oriNew = matrixMult(dRot, currentPiece.ori);
+    for (int i = 0; i < 4; i++) {
+      xNew = currentPiece.xPos + dx + currentPiece.xOff + oriNew[0][i];
+      yNew = currentPiece.yPos + dy + currentPiece.yOff + oriNew[1][i];
+      if (xNew < 0 || xNew > 40) {
+        return true;
+      }
+      if (yNew < 0 || xNew > 80) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  int[][] matrixMult(int[][] A, int[][] B) { //2x2 matrix times 2x4 matrix
+    int[2][4] M;
+    for (int i = 0; i < 8; i++) {
+      M[i/4][i%4] = A[i/4][0]*B[0][i%4] + A[i/4][1]B[1][i%4];
+    }
+    return M;
   }
