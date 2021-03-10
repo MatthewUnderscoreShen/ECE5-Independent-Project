225853c4a230f1507abc6cadf364e95c0d696c73
MatthewUnderscoreShen <MatthewShen02@Gmail.com>
2021-03-10 05:14:52 -0800

    in case I mess up what im about to do

--- TESTBENCH/TESTBENCH.ino
+++ TESTBENCH/TESTBENCH.ino
@@ -1,16 +1,31 @@
 #define joyX A4
 #define joyY A5
+#define LCD_DC 9
+#define LCD_CS 10
 #define buttonJoy 11
-#define buttonA 12
-#define buttonB 13
+#define buttonB 12
+#define buttonA 13
+
+#include "SPI.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_ILI9341.h"
+#include "Fonts/FreeSans9pt7b.h"
+
+Adafruit_ILI9341 lcd = Adafruit_ILI9341(LCD_CS, LCD_DC);
 
 int x, y; //joystick inputs
 int a, b, aLast, bLast; //button inputs
-long atLast, btLast;
+int aDelay, bDelay; //frame delay after pressing a button
+int buttonDelay = 5;
 bool aPress, bPress; //is a or b pressed?
+int DASFrame; //start frame for das
 bool DAS; //has das delay happened yet?
 bool lock; //should the piece be locked?
-int refresh = 1000; //game refresh rate in milliseconds
+int ARE; //entry delay
+int clearDelay = 0;
+bool clearline = false; //clear line?
+int linesToClear; //which lines to clear (it's like a string)
+int refresh = 250; //game refresh rate in milliseconds
 int fallSpeed = 4; //How many frames it takes to fall one block
 int bRefresh = 8; //button press refresh rate
 int pieceNum = 0; //piece number
@@ -27,38 +42,39 @@ struct piece {
   int yOff; //to shift it to the board
   int xPos; //location
   int yPos;
-  int ori[2][4];
+  int startFrame; //frame where piece was entered
+  int ori[2][4]; //piece matrix
 };
 typedef struct piece Piece;
 
 //Initializing pieces.
 // 0:I  1:O  2:T  3:J  4:L  5:S  6:Z
 Piece pieces[] = {
-      (Piece){-1, 1, 10, 38, {
+      (Piece){-1, 1, 10, 38, 0, {
                 {-3, -1, 1, 3}, //x
                 { 1,  1, 1, 1}  //y
              }}, //I piece
-      (Piece){1, 1 , 8, 36, { 
+      (Piece){1, 1 , 8, 36, 0, { 
                 {-1, -1,  1, 1},
                 {-1,  1, -1, 1} 
              }}, //O piece
-      (Piece){0, 0, 8, 36, {
+      (Piece){0, 0, 8, 36, 0, {
                 {0, -2, 0, 2},
                 {0,  0, 2, 0}
              }}, //T piece
-      (Piece){0, 0, 8, 36, {
+      (Piece){0, 0, 8, 36, 0, {
                 {0, -2, -2, 2},
                 {0,  2,  0, 0}
              }}, //J piece
-      (Piece){0, 0, 8, 36, {
+      (Piece){0, 0, 8, 36, 0, {
                 {0, -2, 2, 2},
                 {0,  0, 0, 2}
              }}, //L piece
-      (Piece){0, -1, 8, 36, {
+      (Piece){0, -1, 8, 36, 0, {
                 {-2,    0,   0,   2},
                 {-0.5, -0.5, 0.5, 0.5}
              }}, //S piece
-      (Piece){0, -1, 8, 36, {
+      (Piece){0, -1, 8, 36, 0, {
                 {-2,   0,    0,    2},
                 { 0.5, 0.5, -0.5, -0.5}
              }}  //Z piece
@@ -68,10 +84,11 @@ Piece currentPiece;
 
 int rots[][2][2] = 
   { //Rotation matricies
-    { {1,  0}, { 0, 1} }, //0 deg 
-    { {0, -1}, { 1, 0} }, //90deg ccw around origin
-    { {0,  1}, {-1, 0} }  //90deg cw around origin
+    { {0, -1}, { 1, 0} }, //90deg ccw
+    { {1,  0}, { 0, 1} }, //0deg
+    { {0,  1}, {-1, 0} }  //90deg cw
   };
+
 
   int choosePiece() {
     int r = random(7);
@@ -88,6 +105,7 @@ int rots[][2][2] =
     for (int i = 0; i < 8; i++) {
       currentPiece.ori[i/4][i%4] = pieces[pieceNum].ori[i/4][i%4];
     }
+    currentPiece.startFrame = t/refresh;
 
     if (checkCollision()) {
       Serial.println("Game Over");
@@ -102,15 +120,16 @@ int rots[][2][2] =
   void movePiece(int dx, int dy, int dRot[][2]) {
     if (boundsCheck(dx, dy, dRot)) { //both directions pass
       currentPiece.xPos = currentPiece.xPos + dx;
-      currentPiece.yPos = currentPiece.yPos + (t/refresh%(fallSpeed/dy) == 0 ? -2 : 0);
+      currentPiece.yPos = currentPiece.yPos + ((t/refresh-currentPiece.startFrame)%(fallSpeed/dy) == 0 ? -2 : 0);
       matrixMult(dRot, currentPiece.ori, currentPiece.ori);
-    } else if (boundsCheck(dx, 0, dRot){ //vertical fail
+    } else if (boundsCheck(dx, 0, dRot)){ //vertical fail
       currentPiece.xPos = currentPiece.xPos + dx;
       matrixMult(dRot, currentPiece.ori, currentPiece.ori);
       lock = true;
-    } else if (boundsCheck(0, dy, dRot) { //horizontal fail
+    } else if (boundsCheck(0, dy, dRot)) { //horizontal fail
       currentPiece.yPos = currentPiece.yPos + (t/refresh%(fallSpeed/dy) == 0 ? -2 : 0);
       matrixMult(dRot, currentPiece.ori, currentPiece.ori);
+      //DAS
     } else { //both directions fail
       return;
     }
@@ -118,7 +137,21 @@ int rots[][2][2] =
 
   void checkLock() {
     if (lock) {
-      
+      for (int i = 0; i < 4; i++) {
+        board[currentPiece.xPos + currentPiece.xOff + currentPiece.ori[0][i]][currentPiece.yPos + currentPiece.yOff + currentPiece.ori[1][i]] = 1;
+      }
+      activePiece = false;
+      //ARE = 10 + ((currentPiece.yPos + 2)/4)*2
+    }
+  }
+
+  void checkLineClear() {
+    if (lock) {
+      for (int i = 0; i < 40; i = i + 2) {
+        for (int j = 0; j < 20; j = j + 2) {
+          
+        }
+      }
     }
   }
 
@@ -131,6 +164,30 @@ int rots[][2][2] =
     return false;
   }
 
+  void printBoard() {
+    lcd.drawRect(6, 32, 122, 282, ILI9341_BLACK);
+    for (int i = 38; i >= 0; i = i - 2) {
+      for (int j = 0; j < 20; j = j + 2) {
+        if (currentPiece.xPos + currentPiece.xOff + currentPiece.ori[0][0] == j && currentPiece.yPos + currentPiece.yOff + currentPiece.ori[1][0] == i) {
+          lcd.fillRect(7 + 6*j, 7*(38-i) + 33, 12, 14, ILI9341_YELLOW);
+          lcd.drawRect(7 + 6*j, 7*(38-i) + 33, 12, 14, ILI9341_BLACK);
+        } else if (currentPiece.xPos + currentPiece.xOff + currentPiece.ori[0][1] == j && currentPiece.yPos + currentPiece.yOff + currentPiece.ori[1][1] == i) {
+          lcd.fillRect(7 + 6*j, 7*(38-i) + 33, 12, 14, ILI9341_YELLOW);
+          lcd.drawRect(7 + 6*j, 7*(38-i) + 33, 12, 14, ILI9341_BLACK);
+        } else if (currentPiece.xPos + currentPiece.xOff + currentPiece.ori[0][2] == j && currentPiece.yPos + currentPiece.yOff + currentPiece.ori[1][2] == i) {
+          lcd.fillRect(7 + 6*j, 7*(38-i) + 33, 12, 14, ILI9341_YELLOW);
+          lcd.drawRect(7 + 6*j, 7*(38-i) + 33, 12, 14, ILI9341_BLACK);
+        } else if (currentPiece.xPos + currentPiece.xOff + currentPiece.ori[0][3] == j && currentPiece.yPos + currentPiece.yOff + currentPiece.ori[1][3] == i) {
+          lcd.fillRect(7 + 6*j, 7*(38-i) + 33, 12, 14, ILI9341_YELLOW);
+          lcd.drawRect(7 + 6*j, 7*(38-i) + 33, 12, 14, ILI9341_BLACK);
+        } else {
+          lcd.fillRect(7 + 6*j, 7*(38-i) + 33, 12, 14, 0x3E3C); 
+          lcd.drawRect(7 + 6*j, 7*(38-i) + 33, 12, 14, ILI9341_BLACK);
+        } 
+      }
+    }
+  } 
+/*
   void printBoard() {
     Serial.println("------------------------------");
     for (int i = 38; i >=  0; i = i - 2) {
@@ -153,7 +210,7 @@ int rots[][2][2] =
     }
     Serial.println("------------------------------");
   }
-
+*/
   //Matrix A, Matrix B, the matrix to copy to
   void matrixMult(int A[][2], int B[][4], int C[][4]) { //2x2 matrix times 2x4 matrix
     int M[2][4];
@@ -193,23 +250,47 @@ int rots[][2][2] =
   int getdy(double y) {
     return y > 1000 ? 2 : 1;   
   }
-/*
+
   int getdRots(int aIn, int, alIn, int bIn, int blIn) {
+    int rot = 1;
     if (aIn == 1 && alIn == 0) {
-      return 2;
-    } else if (
+      if (aDelay == 0) {
+        aDelay = buttonDelay;
+        rot = rot + 1;
+      } else {
+        aDelay = aDelay - 1;
+      }
+    }
+    if (bIn == 1 && blIn == 0) {
+      if (bDelay == 0) {
+        bDelay = buttonDelay;
+        rot = rot - 1;
+      } else {
+        bDelay = bDelay - 1;
+      }
+    }
+    return rot;
   }
-*/
+
 //-------------------------------------------------------------------------------
 
   void setup() {
     // put your setup code here, to run once:
     Serial.begin(9600);
+    lcd.begin();
+    uint8_t lcdRead = lcd.readcommand8(ILI9341_RDMODE);
+    lcd.setRotation(2);
     pinMode(buttonA, INPUT);
     pinMode(buttonB, INPUT);
     for (int i = 0; i < 800; i++) { //initalizes the board
       board[i / 20][i % 20] = 0;
     }
+
+    lcd.fillScreen(0x3E3C);
+
+    ARE = 10;
+    aDelay = 0;
+    bDelay = 0;
 
     aLast = 0;
     bLast = 0;
@@ -224,23 +305,16 @@ int rots[][2][2] =
     y = analogRead(joyY);
     a = digitalRead(buttonA);
     b = digitalRead(buttonB);
-
-    if (a == 1 && !aPress && (atLast + 10) == t) {
-      aPress = true;
-    } else if (a == 1 && !aPress && atLast != t) {
-      atLast = t;
-    }
-
-    if (b == 1 && !bPress && (btLast + 10) == t) {
-      aPress = true;
-    } else if (b == 1 && !bPress && btLast != t) {
-      atLast = t;
-    }
     
     //
     //GAME LOOP START--------------------------------------------------------
     //
     if (t % refresh == 0 && tLast != t) { //Game loop
+
+      x = analogRead(joyX);
+      y = analogRead(joyY);
+      a = digitalRead(buttonA);
+      b = digitalRead(buttonB);
 
       //Check if game is active
       if (gameState == false) {
@@ -256,9 +330,11 @@ int rots[][2][2] =
 
       if (!activePiece) { return; }
       
-      movePiece(getdx(x), getdy(y), rots[0]/*rots[getdRot(a, aLast, b, bLast)]*/); //moves piece
+      movePiece(getdx(x), getdy(y), rots[getdRot(a, aLast, b, bLast)]); //moves piece
 
-      checkLock():
+      checkLock();
+
+      //checkLine();
 
       printBoard();
 
@@ -274,7 +350,5 @@ int rots[][2][2] =
     Serial.print("\t");
     Serial.println(y);
 */
-    aLast = a;
-    bLast = b;
     tLast = t;
   }
