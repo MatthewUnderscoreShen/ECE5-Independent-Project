1a608b5f63950c43fa68d791c4403aefee718fbc
MatthewUnderscoreShen <MatthewShen02@Gmail.com>
2021-03-10 08:29:22 -0800

    tentative DAS

--- TESTBENCH/TESTBENCH.ino
+++ TESTBENCH/TESTBENCH.ino
@@ -18,8 +18,9 @@ int a, b, aLast, bLast; //button inputs
 int aDelay, bDelay; //frame delay after pressing a button
 int buttonDelay = 3;
 bool aPress, bPress; //is a or b pressed?
-int DASFrame; //start frame for das
-bool DAS; //has das delay happened yet?
+int DAS; //delayed action shift
+bool isDAS; //Has DAS happened yet?
+int DASCounter; //DAS reset counter
 bool lock; //should the piece be locked?
 int ARE; //entry delay
 int clearDelay = 0;
@@ -29,6 +30,7 @@ int refresh = 16; //game refresh rate in milliseconds
 int fallSpeed = 48; //How many frames it takes to fall one block
 int bRefresh = 8; //button press refresh rate
 int pieceNum = 0; //piece number
+int nextPiece = 0;
 int lastPiece = 7;
 long t = 0;
 long tLast = 0;
@@ -55,29 +57,29 @@ Piece pieces[] = {
                 {-3, -1, 1, 3}, //x
                 { 1,  1, 1, 1}  //y
              }}, //I piece
-      (Piece){1, 1 , 8, 36, ILI9341_YELLOW, 0, { 
+      (Piece){1, 1 , 8, 38, ILI9341_YELLOW, 0, { 
                 {-1, -1,  1, 1},
                 {-1,  1, -1, 1} 
              }}, //O piece
-      (Piece){0, 0, 8, 36, ILI9341_MAGENTA, 0, {
+      (Piece){0, 0, 10, 40, ILI9341_MAGENTA, 0, {
                 {0, -2, 0, 2},
-                {0,  0, 2, 0}
+                {0,  0, -2, 0}
              }}, //T piece
-      (Piece){0, 0, 8, 36, ILI9341_BLUE, 0, {
-                {0, -2, -2, 2},
-                {0,  2,  0, 0}
+      (Piece){0, 0, 10, 40, ILI9341_BLUE, 0, {
+                {0, -2, 2,  2},
+                {0,  0, 0, -2}
              }}, //J piece
-      (Piece){0, 0, 8, 36, ILI9341_ORANGE, 0, {
-                {0, -2, 2, 2},
-                {0,  0, 0, 2}
+      (Piece){0, 0, 10, 40, ILI9341_ORANGE, 0, {
+                {0, -2, -2, 2},
+                {0, -2,  0, 0}
              }}, //L piece
-      (Piece){0, -1, 8, 36, ILI9341_GREENYELLOW, 0, {
-                {-2,    0,   0,   2},
-                {-0.5, -0.5, 0.5, 0.5}
+      (Piece){0, 0, 10, 40, ILI9341_GREENYELLOW, 0, {
+                {0, -2,  0, 2},
+                {0, -2, -2, 0}
              }}, //S piece
-      (Piece){0, -1, 8, 36, ILI9341_RED, 0, {
-                {-2,   0,    0,    2},
-                { 0.5, 0.5, -0.5, -0.5}
+      (Piece){0, 0, 10, 40, ILI9341_RED, 0, {
+                {0, -2,  0,  2},
+                {0,  0, -2, -2}
              }}  //Z piece
   };
 
@@ -89,6 +91,19 @@ int rots[][2][2] =
     { {1,  0}, { 0, 1} }, //0deg
     { {0,  1}, {-1, 0} }  //90deg cw
   };
+
+  void initPrint() {
+    //lcd.fillRect(7, 5, 120, 28, ILI9341_LIGHTGREY); //lazy box
+    
+    lcd.drawRect(146, 32, 82, 62, ILI9341_LIGHTGREY);
+    lcd.drawRect(146, 32, 82, 11, ILI9341_LIGHTGREY);
+    lcd.fillRect(147, 33, 80, 9, ILI9341_YELLOW); 
+    lcd.setFont();
+    lcd.setTextSize(0);
+    lcd.setTextColor(ILI9341_BLACK);
+    lcd.setCursor(176, 34);
+    lcd.print("NEXT");
+  }
 
   void printBoard() {
     lcd.drawRect(6, 32, 122, 282, ILI9341_LIGHTGREY);
@@ -111,14 +126,25 @@ int rots[][2][2] =
     }
   }
 
+  void printNext() {
+    for (int i = 0; i < 4; i++) {
+      lcd.fillRect(121 + 6*(pieces[nextPiece].xPos + pieces[nextPiece].xOff + pieces[nextPiece].ori[0][i]), 
+                   68 + 7*(38-(pieces[nextPiece].yPos + pieces[nextPiece].yOff + pieces[nextPiece].ori[1][i])),
+                   12, 14, pieces[nextPiece].color);
+      lcd.drawRect(121 + 6*(pieces[nextPiece].xPos + pieces[nextPiece].xOff + pieces[nextPiece].ori[0][i]), 
+                   68 + 7*(38-(pieces[nextPiece].yPos + pieces[nextPiece].yOff + pieces[nextPiece].ori[1][i])),
+                   12, 14, ILI9341_LIGHTGREY);
+    }
+  }
+
   int choosePiece() {
     int r = random(7);
-    return ( (r == 7 || r == lastPiece) ? random(6) : r );
+    return ( (r == 7 || r == pieceNum) ? random(6) : r );
   }
 
   void setPiece() { //this function chooses and sets the piece
-    if (activePiece) { return; }
-    pieceNum = choosePiece();
+    pieceNum = nextPiece;
+    nextPiece = choosePiece();
     currentPiece.xOff = pieces[pieceNum].xOff;
     currentPiece.yOff = pieces[pieceNum].yOff;
     currentPiece.xPos = pieces[pieceNum].xPos;
@@ -136,18 +162,19 @@ int rots[][2][2] =
     }
 
     activePiece = true;
-    lastPiece = pieceNum;
+    printNext();
   }
 
   void movePiece(int dx, int dy, int dRot) {
     if (dx == 0 && ((t/refresh-currentPiece.startFrame)%(fallSpeed/dy) != 0) && dRot == 1) {
+      DASCounter = DASCounter + 1;
       return;
     }
     
     if (boundsCheck(dx, dy, rots[dRot])) { //both directions pass
       printPiece(ILI9341_BLACK);
       currentPiece.xPos = currentPiece.xPos + dx;
-      currentPiece.yPos = currentPiece.yPos + ((t/refresh-currentPiece.startFrame)%(fallSpeed/dy) == 0 ? -2 : 0);
+      currentPiece.yPos = currentPiece.yPos + dy;
       matrixMult(rots[dRot], currentPiece.ori, currentPiece.ori);
       printPiece(currentPiece.color);
     } else if (boundsCheck(dx, 0, rots[dRot])){ //vertical fail
@@ -158,11 +185,16 @@ int rots[][2][2] =
       printPiece(currentPiece.color);
     } else if (boundsCheck(0, dy, rots[dRot])) { //horizontal fail
       printPiece(ILI9341_BLACK);
-      currentPiece.yPos = currentPiece.yPos + (t/refresh%(fallSpeed/dy) == 0 ? -2 : 0);
+      currentPiece.yPos = currentPiece.yPos + dy;
       matrixMult(rots[dRot], currentPiece.ori, currentPiece.ori);
-      //DAS
       printPiece(currentPiece.color);
-    } else { //both directions fail
+      DASCounter = DASCounter + 1;
+    } else if (boundsCheck(0, 0, rots[dRot]){ //only rotation
+      printPiece(ILI9341_BLACK);
+      matrixMult(rots[dRot], currentPiece.ori, currentPiece.ori);
+      printPiece(currentPiece.color);
+      DASCounter = DASCounter + 1;
+    } else { //total failure
       return;
     }
   }
@@ -214,7 +246,7 @@ int rots[][2][2] =
     matrixMult(dRot, currentPiece.ori, oriNew);
     for (int i = 0; i < 4; i++) {
       xNew = currentPiece.xPos + dx + currentPiece.xOff + oriNew[0][i];
-      yNew = currentPiece.yPos + (t/refresh%(fallSpeed/dy) == 0 ? -2 : 0) + currentPiece.yOff + oriNew[1][i];
+      yNew = currentPiece.yPos + dy + currentPiece.yOff + oriNew[1][i];
       if (xNew < 0 || xNew > 18) { return false; }
       if (yNew < 0 || yNew > 40) { return false; }
       if (board[xNew][yNew] != 0) { return false; }
@@ -223,33 +255,32 @@ int rots[][2][2] =
   }
 
   int getdx(double x) {
-    if (x < 450) {
-      return 2;
-    } else if (x > 1000) {
-      return -2;
-    } else {
-      return 0;
+    if (DAS == 0) { //if DAS == 0, then the piece can move
+      DAS = isDAS ? 6 : 16; //if das occured, 6 frame delay. otherwise, 16 frame delay
+      DAS = true; //das has now occurred
+      return (x < 450) ? 2 : ( (x > 1000) ? -2 : 0 );
     }
+    DAS = DAS - 1;
   }
 
+//GRAVITY IS FINISHED
   int getdy(double y) {
-    return y > 1000 ? 2 : 1;   
+    return (t/refresh-currentPiece.startFrame)%(fallSpeed/(y > 1000 ? 2 : 1)) == 0 ? -2 : 0
   }
 
-  int getdRots(int aIn, int alIn, int bIn, int blIn) {
+//ROTATION IS FINISHED
+  int getdRots(int aIn, int alIn, int bIn, int blIn) { //gets the rotation matrix
     int rot = 1;
     if (aIn > 0 && alIn == 0) {
       if (aDelay == 0) {
         aDelay = buttonDelay;
         rot = rot + 1;
-        Serial.println("cw");
       }
     }
     if (bIn > 0 && blIn == 0) {
       if (bDelay == 0) {
         bDelay = buttonDelay;
         rot = rot - 1;
-        Serial.println("ccw");
       }
     }
     aDelay = (aDelay == 0) ? 0 : aDelay - 1;
@@ -263,6 +294,7 @@ int rots[][2][2] =
     // put your setup code here, to run once:
     Serial.begin(9600);
     lcd.begin();
+    randomSeed(analogRead(A0));
     uint8_t lcdRead = lcd.readcommand8(ILI9341_RDMODE);
     lcd.setRotation(2);
     pinMode(buttonA, INPUT);
@@ -273,8 +305,14 @@ int rots[][2][2] =
 
     lcd.fillScreen(ILI9341_BLACK);
     printBoard();
+    initPrint();
+
+    nextPiece = random(6);
 
     ARE = 10;
+    DAS = 0;
+    isDAS = false;
+    DASCounter = 0;
     aDelay = 0;
     bDelay = 0;
 
@@ -307,20 +345,14 @@ int rots[][2][2] =
         return;
       }
 
-      setPiece(); //Places piece on board
-
-      if (!activePiece) { return; }
-
-      //printPiece(ILI9341_BLACK);
+      if (!activePiece) { 
+        setPiece(); 
+      } 
+      else {
+        movePiece(getdx(x), getdy(y), getdRots(a, aLast, b, bLast)); //moves piece
+        checkLock(); //check if the piece should lock
+      }
       
-      movePiece(getdx(x), getdy(y), getdRots(a, aLast, b, bLast)); //moves piece
-
-      checkLock();
-
-      //checkLine();
-
-      //printPiece(currentPiece.color);
-
       aLast = a;
       bLast = b;
       a = 0;
